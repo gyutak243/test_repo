0eb1c0ff8748b071f8e450d5fd071599dea17673
gyutak243 <103906840+gyutak243@users.noreply.github.com>
2026-03-12 22:08:14 +0900

    이름, 학번, 전공 추가

--- gcd_test.py
+++ gcd_test.py
@@ -1,9 +1,13 @@
+## 박규탁, 202300328, 컴퓨터공학부
+
 def gcd_sub(a, b):
     while(a != 0 and b != 0):
         if a>b:
             a = a-b
         elif b>a:
             b = b-a
+        elif a == b:
+            a = a-b
     return a+b
 def gcd_mod(a, b):
     while(a != 0 and b != 0):
@@ -11,6 +15,8 @@ def gcd_mod(a, b):
             a = a%b
         elif b>a:
             b = b%a
+		elif a == b:
+            a = a-b
     return a+b
 
 def gcd_rec(a, b):
@@ -23,4 +29,5 @@ x = gcd_sub(a,b)
 y = gcd_mod(a,b)
 z = gcd_rec(a,b)
 # gcd_sub, gcd_mod, gcd_rec을 각각 호출하여, x, y, z에 리턴값을 저장한다
+
 print(x, y, z)
